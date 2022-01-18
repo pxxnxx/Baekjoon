5ce4893c30e8a671021d2c57678f780f87b4dfb5
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-01-18 14:37:47 +0900

    줄 세우기

---
+++ TopologyOrder_2252.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n, m = map(int,input().split())
+arr = [[] for _ in range(n+1)]
+deg = [0 for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int,input().split())
+    arr[a].append(b)
+    deg[b] += 1
+queue = deque()
+for i in range(1, n+1):
+    if deg[i] == 0:
+        queue.append(i)
+answer = []
+while queue:
+    index = queue.popleft()
+    answer.append(index)
+    for x in arr[index]:
+        deg[x] -= 1
+        if deg[x] == 0:
+            queue.append(x)
+print(*answer,sep=' ')
+
