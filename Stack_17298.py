bc5cf872db0155a37a1c46a9b77f6c36291c5961
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-02-27 14:50:37 +0900

    오큰수

---
+++ Stack_17298.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int,input().split()))
+stack = [0]
+answer = [-1 for _ in range(n)]
+for i in range(1, n):
+    while stack and arr[stack[-1]] < arr[i]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+print(*answer,sep=' ')
