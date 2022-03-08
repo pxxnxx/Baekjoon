e57cd3cda4e7566190d91391099ad116b78c4512
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-03-09 01:14:54 +0900

    수열과 시프트 쿼리

---
+++ Simulation_17499.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n, q = map(int,input().split())
+arr = list(map(int,input().split()))
+p = 0
+for i in range(q):
+    query = list(map(int,input().split()))
+    if query[0] == 1:
+        arr[(p+query[1]-1)%n] += query[2]
+    elif query[0] == 2:
+        p -= query[1]
+    else:
+        p += query[1]
+for i in range(p, p+n):
+    print(arr[i%n], end=' ')
