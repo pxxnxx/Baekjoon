b7da4978138953d9e405164b407fe9f1dec3a685
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-05-14 01:06:09 +0900

    부분 문자열

---
+++ KMP_16916.py
@@ -0,0 +1,29 @@
+def getPS():
+    ps = [0] * m
+    j = 0
+    for i in range(1, m):
+        while 0 < j and brr[i] != brr[j]:
+            j = ps[j-1]
+        if brr[i] == brr[j]:
+            j += 1
+            ps[i] = j
+    return ps
+
+def kmp():
+    ps = getPS()
+    j = 0
+    for i in range(n):
+        while 0 < j and arr[i] != brr[j]:
+            j = ps[j-1]
+        if arr[i] == brr[j]:
+            if j == m-1:
+                return 1
+            else:
+                j += 1
+    return 0
+
+arr = str(input())
+brr = str(input())
+n = len(arr)
+m = len(brr)
+print(kmp())
